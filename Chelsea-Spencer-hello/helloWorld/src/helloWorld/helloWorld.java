b630427b3303e3d506efc71d89df29a8041772d6
Chelsearspencer <71734078+Chelsearspencer@users.noreply.github.com>
2021-09-14 20:08:31 -0400

    hello world chelsea

---
+++ Chelsea-Spencer-hello/helloWorld/src/helloWorld/helloWorld.java
@@ -0,0 +1,14 @@
+package helloWorld;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+System.out.print("Hello World");
+		
+		
+		
+	}
+
+}
